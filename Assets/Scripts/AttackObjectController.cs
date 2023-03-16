bd905936e03b46fa90efeab6a28e0895e567391d
France30 <53886711+France30@users.noreply.github.com>
2023-03-16 15:50:22 +0800

    AttackObjectController: limit max object scale  - Limit max proximity attack scale  - Disable attack when scale is equal to or exceeds the assigned max scale and reset the local scale

--- Assets/Scripts/AttackObjectController.cs
+++ Assets/Scripts/AttackObjectController.cs
@@ -6,16 +6,26 @@ public class AttackObjectController : MonoBehaviour
 {
     //might replace variables with scriptable objects
     //using this format for prototype
+    [SerializeField] private float maxScale = 2f;
     [SerializeField] private float animationSpeed = 4f;
+    private Vector3 originalScale;
 
     public float AnimationSpeed { get; set; }
 
     private void Awake()
     {
+        originalScale = transform.localScale;
     }
 
     private void Update()
     {
+        //when the object's local scale exceeds the assigned max scale
+        if (transform.localScale.x >= maxScale && transform.localScale.y >= maxScale)
+        {
+            transform.localScale = originalScale;
+            gameObject.SetActive(false);
+        }
+
         //scale object size here, multiplied by animation speed
         float objectScale = animationSpeed * Time.deltaTime;
         Vector3 scaleChange = new Vector3(objectScale, objectScale, 0);
