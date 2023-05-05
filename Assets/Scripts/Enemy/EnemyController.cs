7073e6728710e9b6e2086111695ae0a3480e99db
France30 <53886711+France30@users.noreply.github.com>
2023-05-05 11:55:43 +0800

    Change EnemyController to an Abstract Class

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -2,30 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public abstract class EnemyController : MonoBehaviour
 {
     [SerializeField] private int health = 1;
 
     public bool IsHit { get; set; }
 
-    public void TakeDamage(int value)
+    public virtual void TakeDamage(int value)
     {
         health -= value;
         IsHit = true;
         Debug.Log(gameObject.name + " has been hit");
 
         if (health <= 0) gameObject.SetActive(false);
-    }
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
