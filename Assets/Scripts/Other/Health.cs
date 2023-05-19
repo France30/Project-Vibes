26133fca9d44d2f134be006711bafdf3aa13f667
France30 <53886711+France30@users.noreply.github.com>
2023-05-20 05:45:08 +0800

    Add "Health" Class

---
+++ Assets/Scripts/Other/Health.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health
+{ 
+    private float _currentHealth;
+    private Image _healthBar;
+
+    public float MaxHealth { get; set; }
+    public float CurrentHealth
+    {
+        get => _currentHealth;
+        set
+        {
+            _currentHealth = value;
+            _currentHealth = Mathf.Clamp(_currentHealth, 0, MaxHealth);
+            OnHealthUpdated?.Invoke(_currentHealth, MaxHealth);
+
+            if (_healthBar != null) UpdateHealthBar();
+        }
+    }
+
+    public delegate void HealthUpdate(float health, float maxHealth);
+    public event HealthUpdate OnHealthUpdated;
+
+
+    public Health(float maxHealth, Image healthBar = null)
+    {
+        this._healthBar = healthBar;
+        MaxHealth = maxHealth;
+        CurrentHealth = MaxHealth;
+    }
+
+    private void UpdateHealthBar()
+    {
+        _healthBar.fillAmount = _currentHealth / MaxHealth;
+    }
+}
