45ee78a5440e9c27cfd51496e6b6d22193fbc65b
France30 <53886711+France30@users.noreply.github.com>
2023-05-24 15:20:50 +0800

    GroundEnemy: Add "Patrol" Behaviour  - enemy follows a set of way points

--- Assets/Scripts/Enemy/GroundEnemy.cs
+++ Assets/Scripts/Enemy/GroundEnemy.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(CharacterController2D))]
 public class GroundEnemy : EnemyBase
 {
+    [SerializeField] private Transform[] _wayPoints;
+
     private CharacterController2D _controller;
     private Rigidbody2D _rb2D;
 
+    private int _currentWayPoint = 0;
 
     protected override void Awake()
     {
@@ -20,5 +23,26 @@ public class GroundEnemy : EnemyBase
     {
         base.FixedUpdate();
         _controller.Move(_moveSpeed * Time.fixedDeltaTime, false, false);
+
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        Transform currentPatrol = _wayPoints[_currentWayPoint];
+        float distanceFromTarget = transform.position.x - currentPatrol.position.x;
+        bool isTargetAbove = currentPatrol.position.y > transform.position.y;
+        if (IsTargetReached(distanceFromTarget) && !isTargetAbove)
+            GoToNextWayPoint();
+
+        MoveToTargetDirection(currentPatrol);   
+    }
+
+    private void GoToNextWayPoint()
+    {
+        _currentWayPoint++;
+
+        if (_currentWayPoint > _wayPoints.Length - 1)
+            _currentWayPoint = 0;
     }
 }
