a7c5251bdcd725aaef57952f367427d07b946e66
France30 <53886711+France30@users.noreply.github.com>
2023-05-30 07:17:18 +0800

    GroundEnemy: Update Movement Behaviour

--- Assets/Scripts/Enemy/GroundEnemy.cs
+++ Assets/Scripts/Enemy/GroundEnemy.cs
@@ -8,6 +8,7 @@ public class GroundEnemy : EnemyBase
     [SerializeField] private GroundEnemyType _groundEnemyType;
     [SerializeField] private Transform _ceilingCheck;
     [SerializeField] private Transform _wallCheck;
+    [SerializeField] private Transform _groundPlatformCheck;
     [SerializeField] private LayerMask _whatIsPlatform;
     [SerializeField] private float _fallingThreshold = -5f;
 
@@ -15,7 +16,7 @@ public class GroundEnemy : EnemyBase
     private Rigidbody2D _rb2D; 
 
     private Vector2 _ceilingBoxCastSize = new Vector2(1.5f, 1f);
-    private Vector2 _wallBoxCastSize;
+    private Vector2 _localScale;
 
     private bool _canJump = false;
     private bool _isGrounded = true;
@@ -23,14 +24,54 @@ public class GroundEnemy : EnemyBase
     public delegate void OnEnemyJump();
     public event OnEnemyJump OnEnemyJumpEvent;
 
+
     public void OnLanding()
     {
         _isGrounded = true;
     }
 
+    public override void MoveToTargetDirection(Transform target)
+    {
+        _groundEnemyType.CurrentTarget = target;
+
+        if (_groundEnemyType.JumpCondition())
+            Jump();
+
+        if (IsTargetOnPlatform(target) || IsTargetBelowPlatform(target)) return;
+
+        bool isFalling = _rb2D.velocity.y < _fallingThreshold;
+        if (isFalling || _isGrounded)
+            base.MoveToTargetDirection(target);
+    }
+
+    private bool IsTargetOnPlatform(Transform target)
+    {
+        bool isTargetAbove = target.position.y > transform.position.y;
+        if (isTargetAbove)
+        {
+            bool isBelowPlatform = Physics2D.BoxCast(_ceilingCheck.position, _ceilingBoxCastSize, 0, transform.up, Mathf.Infinity, _whatIsPlatform);
+            if (isBelowPlatform) return true;
+        }
+
+        return false;
+    }
+
+    private bool IsTargetBelowPlatform(Transform target)
+    {
+        bool isTargetBelow = target.position.y < transform.position.y;
+        if (isTargetBelow)
+        {
+            bool isOnPlatform = Physics2D.BoxCast(_groundPlatformCheck.position, _localScale, 0, _groundPlatformCheck.position, Mathf.Infinity, _whatIsPlatform);
+            if (isOnPlatform) return true;
+        }
+
+        return false;
+    }
+
     protected override void Awake()
     {
         base.Awake();
+
         _controller = GetComponent<CharacterController2D>();
         _rb2D = GetComponent<Rigidbody2D>();
 
@@ -38,8 +79,8 @@ public class GroundEnemy : EnemyBase
 
         _groundEnemyType.InitializeEnemy(this);
 
-        _wallBoxCastSize = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
-        _groundEnemyType.InitializeChecks(_wallCheck, _ceilingCheck, _wallBoxCastSize, _ceilingBoxCastSize, _whatIsPlatform);
+        _localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        _groundEnemyType.InitializeChecks(_wallCheck, _ceilingCheck, _localScale, _ceilingBoxCastSize, _whatIsPlatform);
     }
 
     protected override void FixedUpdate()
@@ -52,27 +93,6 @@ public class GroundEnemy : EnemyBase
             _controller.Move(0, false, _canJump);
 
         _canJump = false;
-    }
-
-    protected override void MoveToTargetDirection(Transform target)
-    {
-        _groundEnemyType.CurrentTarget = target;
-
-        if (_groundEnemyType.JumpCondition())
-            Jump();
-
-        //Do not update direction if target is on a platform and enemy is underneath the same platform
-        //This is so the enemy doesn't get stuck since ground enemies only follow the x position of the target
-        bool isTargetAbove = target.position.y > transform.position.y;
-        if (isTargetAbove)
-        {
-            bool isBelowPlatform = Physics2D.BoxCast(_ceilingCheck.position, _ceilingBoxCastSize, 0, transform.up, Mathf.Infinity, _whatIsPlatform);
-            if (isBelowPlatform) return;
-        }
-
-        bool isFalling = _rb2D.velocity.y < _fallingThreshold;
-        if (isFalling || _isGrounded)
-            base.MoveToTargetDirection(target);
     }
 
     private void Jump()
