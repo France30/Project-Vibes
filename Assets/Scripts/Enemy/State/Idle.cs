4e93cca2713d75ab9f9013dd0609244812312ba8
France30 <53886711+France30@users.noreply.github.com>
2023-06-12 16:23:52 +0800

    Add Idle State Behaviour

--- Assets/Scripts/Enemy/State/Idle.cs
+++ Assets/Scripts/Enemy/State/Idle.cs
@@ -3,5 +3,25 @@ using UnityEngine;
 
 public class Idle : State
 {
+    [SerializeField] private float _idleTime = 3f;
 
+
+    private float _currentTime = 0f;
+
+
+    public override void PerformState()
+    {
+        base.PerformState();
+
+        if (_currentTime < _idleTime)
+            _currentTime += Time.fixedDeltaTime;
+    }
+
+
+    private void LateUpdate()
+    {
+        //Reset timer
+        if (_enemyBase.CurrentState != this) 
+            _currentTime = 0;
+    }
 }
