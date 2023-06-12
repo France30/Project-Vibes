094598da92112d78a200c71f47119033db9a5bdd
France30 <53886711+France30@users.noreply.github.com>
2023-06-12 15:54:05 +0800

    Remove comment

--- Assets/Scripts/Enemy/Ground Enemies/NormalGround.cs
+++ Assets/Scripts/Enemy/Ground Enemies/NormalGround.cs
@@ -6,7 +6,6 @@ public class NormalGround : GroundEnemy
     {
         if (CurrentTarget == null) return false;
 
-        //Only move if not on target and not attacking
         return !IsTargetReached(CurrentTarget) && !IsAttacking;
     }
 
