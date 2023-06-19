1c430a815d3b7cb78385a85df4730cca76bcaeda
France30 <53886711+France30@users.noreply.github.com>
2023-06-20 07:58:15 +0800

    Add SpriteFlash ScriptableObject

---
+++ Assets/Scripts/Other/Entities/SpriteFlash.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sprite Flash", menuName = "Configs/SpriteController")]
+public class SpriteFlash : ScriptableObject
+{
+    [SerializeField] private float _flashSpeed;
+    [SerializeField] private int _flashCount;
+    [SerializeField] private Material _flashMaterial;
+
+    public float FlashSpeed { get { return _flashSpeed; } }
+    public int FlashCount { get { return _flashCount; } }
+    public Material FlashMaterial { get { return _flashMaterial; } }
+}
