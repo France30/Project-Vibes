42be983473055a6e26bf16b5e80c68dd963bf8de
France30 <53886711+France30@users.noreply.github.com>
2023-06-25 18:23:06 +0800

    Add Player Interaction For Player To Interact With Checkpoint

---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] private float _interactRadius = 2f;
+    [SerializeField] private LayerMask _whatIsInteractable;
+
+    private Interactable _interactable;
+    private CircleCollider2D[] _interactCollider = new CircleCollider2D[1];
+
+
+    private void Update()
+    {
+        if (_interactable == null) return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+            _interactable.Interact();
+    }
+
+    private void FixedUpdate()
+    {
+        int interactables = Physics2D.OverlapCircleNonAlloc(transform.position, _interactRadius, _interactCollider, _whatIsInteractable);
+        if (interactables > 0 && _interactable == null)
+        {
+            _interactable = _interactCollider[0].GetComponent<Interactable>();
+            _interactable.InteractableText.enabled = true;
+        }
+        else if (interactables <= 0 && _interactable != null)
+        {
+            _interactable.InteractableText.enabled = false;
+            _interactable = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _interactRadius);
+    }
+}
