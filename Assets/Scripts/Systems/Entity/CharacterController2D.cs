e32c24a510897b6aaa1bb70e3a10b4418028a39a
France30 <53886711+France30@users.noreply.github.com>
2023-06-29 19:01:44 +0800

    Prevent preemptive "OnLandEvent" by checking if the character is jumping

--- Assets/Scripts/Systems/Entity/CharacterController2D.cs
+++ Assets/Scripts/Systems/Entity/CharacterController2D.cs
@@ -14,6 +14,7 @@ public class CharacterController2D : MonoBehaviour
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
+	private bool _isJumping = false;
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
@@ -46,6 +47,14 @@ public class CharacterController2D : MonoBehaviour
 	{
 		bool wasGrounded = m_Grounded;
 		m_Grounded = false;
+
+		//prevent preemptive "OnLandEvent" call by checking if character is jumping
+		if (_isJumping)
+		{
+			//Character is jumping if the velocity is rising
+			_isJumping = (m_Rigidbody2D.velocity.y >= 0);
+			return;
+		}
 
 		int colliders = Physics2D.OverlapCircleNonAlloc(m_GroundCheck.position, k_GroundedRadius, _groundCollider, m_WhatIsGround);
 		for (int i = 0; i < colliders; i++)
@@ -130,6 +139,8 @@ public class CharacterController2D : MonoBehaviour
 
 			m_Rigidbody2D.velocity = new Vector2(0, 0); //Reset the velocity to prevent unintentional boosts
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce)); // Add a vertical force to the player.
+
+			_isJumping = true;
 		}
 	}
 
