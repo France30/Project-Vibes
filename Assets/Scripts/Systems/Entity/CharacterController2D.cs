64320b6332bf45474f78d9c6a4048e4fad3c7840
France30 <53886711+France30@users.noreply.github.com>
2023-06-30 15:07:32 +0800

    Add whitespacing

--- Assets/Scripts/Systems/Entity/CharacterController2D.cs
+++ Assets/Scripts/Systems/Entity/CharacterController2D.cs
@@ -35,6 +35,7 @@ public class CharacterController2D : MonoBehaviour
 	public BoolEvent OnCrouchEvent;
 	private bool m_wasCrouching = false;
 
+
 	private void Awake()
 	{
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
