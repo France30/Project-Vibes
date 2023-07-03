20f4249c9c9ae6f18621dee9ab067cc2f20727da
France30 <53886711+France30@users.noreply.github.com>
2023-07-03 23:26:55 +0800

    Add Interactable MusicSheet

---
+++ Assets/Scripts/Gameplay/ChordSystem/MusicSheet/MusicSheet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSheet : Interactable
+{
+    [SerializeField] private MusicSheetSO _musicSheetSO;
+
+
+    public override void Interact()
+    {
+        PlayerAttack playerAttack = GameController.Instance.Player.GetComponent<PlayerAttack>();
+        playerAttack.enabled = false;
+
+        PlayerChords playerChords = playerAttack.PlayerChords;
+        playerChords.AddToChordSet(_musicSheetSO);
+        playerAttack.enabled = true;
+
+        _musicSheetSO.SaveSheet();
+        gameObject.SetActive(false);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        gameObject.SetActive(_musicSheetSO.IsSheetInScene);
+    }
+}
