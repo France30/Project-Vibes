94efb8aca6eb310bb708fadcb4ef1afefaacffea
France30 <53886711+France30@users.noreply.github.com>
2023-07-13 05:13:49 +0800

    Add Option To Start A New Game  - Starting a new game clears existing save data and loads starting level

--- Assets/Scripts/Menu/ButtonEvent.cs
+++ Assets/Scripts/Menu/ButtonEvent.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Button Event", menuName = "UI/Button Events")]
 public class ButtonEvent : ScriptableObject
 {
+    public void NewGame()
+    {
+        SaveSystem.ClearAllSaveData();
+        LevelManager.Instance.LoadLevelSelect(1);
+        AudioManager.Instance.Stop("MainMenuBGM");
+    }
+
     public void ResetLevel()
     {
         LevelManager.Instance.ResetLevel();
