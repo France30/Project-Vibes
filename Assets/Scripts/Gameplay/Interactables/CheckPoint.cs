4b504963199a68b4593874a242660c84a9f1305c
France30 <53886711+France30@users.noreply.github.com>
2023-07-15 06:22:11 +0800

    Prevent Audio Memory Leak By Disregarding Checkpoint Interaction During Sfx

--- Assets/Scripts/Gameplay/Interactables/CheckPoint.cs
+++ Assets/Scripts/Gameplay/Interactables/CheckPoint.cs
@@ -7,6 +7,8 @@ public class CheckPoint : Interactable
 {
     public override void Interact()
     {
+        if (AudioManager.Instance.IsPlaying("GameSavedSfx")) return;
+
         SaveSystem.SavePlayerData();
         AudioManager.Instance.Play("GameSavedSfx");
         Debug.Log("Game Saved!");
