691ca38337b78539e8efa7e0f9f837779ed6a665
France30 <53886711+France30@users.noreply.github.com>
2023-07-17 16:28:40 +0800

    Remove unused property

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -34,7 +34,6 @@ public abstract class EnemyBase : StateMachine, IDamageable
 
     public GameObject GameObject { get { return gameObject; } }
     public int InstanceID { get { return _instanceID; } }
-    public int MaxHealth { get { return (int)_health.MaxHealth; } }
     protected bool IsAttacking { get; private set; }
     protected bool IsIdle { get; private set; }
 
