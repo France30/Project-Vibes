1a3a1e2bb267c914459262b18cfaa025a90255a2
France30 <53886711+France30@users.noreply.github.com>
2023-07-18 22:01:21 +0800

    Add LevelTransition

---
+++ Assets/Scripts/Gameplay/LevelAssets/LevelTransition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class LevelTransition : MonoBehaviour
+{
+    [SerializeField] private int _transitionToLevel = 1;
+
+    private void Awake()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<Player>())
+        {
+            Time.timeScale = 0;
+            LevelManager.Instance.LoadLevelSelect(_transitionToLevel);
+        }
+    }
+}
