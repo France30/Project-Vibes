d1cba46c98f5289c2599c76c4888bfe7e143d795
France30 <53886711+France30@users.noreply.github.com>
2023-07-20 16:33:13 +0800

    Add EnemyHealthDrop

---
+++ Assets/Scripts/Gameplay/Interactables/HealthDrop/EnemyHealthDrop.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+public class EnemyHealthDrop : MonoBehaviour
+{
+    [SerializeField] private int _amountToRestore = 1;
+
+    private void Awake()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            player.RecoverHealth(_amountToRestore);
+            ObjectPoolManager.Instance.DespawnGameObject(gameObject);
+        }
+    }
+}
