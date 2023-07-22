0d669a505eb89dd41b6199be4910d4635e839432
France30 <53886711+France30@users.noreply.github.com>
2023-07-23 06:48:25 +0800

    Enable "On" Animation If Checkpoint Is Last Saved Checkpoint

--- Assets/Scripts/Gameplay/Interactables/CheckPoint.cs
+++ Assets/Scripts/Gameplay/Interactables/CheckPoint.cs
@@ -1,11 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
 public class CheckPoint : Interactable
 {
+    [SerializeField] private string _id;
+
+    private Animator _animator;
+
     public override void Interact()
     {
         if (AudioManager.Instance.IsPlaying("GameSavedSfx")) return;
 
         SaveSystem.SavePlayerData();
         SaveSystem.SaveCheckpointData(_id);
+        _animator.SetBool("On", true);
         AudioManager.Instance.Play("GameSavedSfx");
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        bool isThisPlayerSpawnPoint = (_id == SaveSystem.LoadCheckpointData());
+        _animator.SetBool("On", isThisPlayerSpawnPoint);
+    }
+
+    private void OnBecameVisible()
+    {
+        bool isThisPlayerSpawnPoint = (_id == SaveSystem.LoadCheckpointData());
+        _animator.SetBool("On", isThisPlayerSpawnPoint);
     }
 }
